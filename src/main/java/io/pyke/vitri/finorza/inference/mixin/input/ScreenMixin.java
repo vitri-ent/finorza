12929d1e234348edf830d1c94d48662396dfdb64
Carson M. <carson@pyke.io>
2024-01-21 10:55:29 -0600

    refactor: use glfw constants everywhere

--- src/main/java/io/pyke/vitri/finorza/inference/mixin/input/ScreenMixin.java
+++ src/main/java/io/pyke/vitri/finorza/inference/mixin/input/ScreenMixin.java
@@ -3,6 +3,7 @@ package io.pyke.vitri.finorza.inference.mixin.input;
 import io.pyke.vitri.finorza.inference.api.KeyboardHandlerAccessor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
@@ -11,6 +12,6 @@ public class ScreenMixin {
     @Overwrite // need this override for container GUIs
     public static boolean hasShiftDown() {
         final KeyboardHandlerAccessor handler = ((KeyboardHandlerAccessor) Minecraft.getInstance().keyboardHandler);
-        return handler.vitri$isKeyPressed(340) || handler.vitri$isKeyPressed(344);
+        return handler.vitri$isKeyPressed(GLFW.GLFW_KEY_LEFT_SHIFT) || handler.vitri$isKeyPressed(GLFW.GLFW_KEY_RIGHT_SHIFT);
     }
 }
