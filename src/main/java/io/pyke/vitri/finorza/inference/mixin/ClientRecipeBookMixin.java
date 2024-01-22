8ba0aae55815ed1def361993c351f4b5ce362d20
Carson M. <carson@pyke.io>
2024-01-21 22:39:35 -0600

    fix: crashes in inventory screens with recipe book

--- src/main/java/io/pyke/vitri/finorza/inference/mixin/ClientRecipeBookMixin.java
+++ src/main/java/io/pyke/vitri/finorza/inference/mixin/ClientRecipeBookMixin.java
@@ -1,37 +1,45 @@
 package io.pyke.vitri.finorza.inference.mixin;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import net.minecraft.client.ClientRecipeBook;
 import net.minecraft.client.RecipeBookCategories;
 import net.minecraft.world.item.crafting.Recipe;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(ClientRecipeBook.class)
 public class ClientRecipeBookMixin {
-    @Inject(
-            method = "categorizeAndGroupRecipes(Ljava/lang/Iterable;)Ljava/util/Map;",
-            at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Ljava/util/ArrayList;add(Ljava/lang/Object;)Z"),
-            locals = LocalCapture.CAPTURE_FAILHARD
-    )
-    private static void injectCategorize(
-            Iterable<Recipe<?>> iterable,
-            CallbackInfoReturnable<Map<RecipeBookCategories, List<List<Recipe<?>>>>> cir,
-            ArrayList<Recipe<?>> list
-    ) {
-        list.sort(Comparator.comparing(r -> r.getResultItem().toString()));
-    }
-
-    @Inject(method = "categorizeAndGroupRecipes(Ljava/lang/Iterable;)Ljava/util/Map;", at = @At("RETURN"), cancellable = true)
-    private static void injectSorted(CallbackInfoReturnable<Map<RecipeBookCategories, List<List<Recipe<?>>>>> cir) {
-        final Map<RecipeBookCategories, List<List<Recipe<?>>>> map = cir.getReturnValue();
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    private static Map<RecipeBookCategories, List<List<Recipe<?>>>> categorizeAndGroupRecipes(Iterable<Recipe<?>> iterable) {
+        HashMap<RecipeBookCategories, List<List<Recipe<?>>>> map = Maps.newHashMap();
+        HashBasedTable table = HashBasedTable.create();
+        for (Recipe<?> recipe : iterable) {
+            if (recipe.isSpecial()) continue;
+            RecipeBookCategories recipeBookCategories2 = ClientRecipeBook.getCategory(recipe);
+            String string = recipe.getGroup();
+            if (string.isEmpty()) {
+                map.computeIfAbsent(recipeBookCategories2, recipeBookCategories -> Lists.newArrayList()).add(
+                        ImmutableList.of(recipe));
+                continue;
+            }
+            ArrayList<Recipe<?>> list = (ArrayList<Recipe<?>>)table.get((Object)recipeBookCategories2, string);
+            if (list == null) {
+                list = Lists.newArrayList();
+                table.put(recipeBookCategories2, string, list);
+                map.computeIfAbsent(recipeBookCategories2, recipeBookCategories -> Lists.newArrayList()).add(list);
+            }
+            list.add(recipe);
+            list.sort(Comparator.comparing(r -> r.getResultItem().toString()));
+        }
         for (final List<List<Recipe<?>>> recipeList : map.values()) {
             for (final List<Recipe<?>> subList : recipeList) {
                 if (subList.size() > 1) {
@@ -40,6 +48,6 @@ public class ClientRecipeBookMixin {
             }
             recipeList.sort(Comparator.comparing(r -> r.get(0).getResultItem().getItem().toString()));
         }
-        cir.setReturnValue(map);
+        return map;
     }
 }
